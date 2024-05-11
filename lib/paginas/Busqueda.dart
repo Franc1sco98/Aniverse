04f473bf2d7b02205de6a807caf1bac76e3ce146
Franc1sco98 <panchojaimeslopes53@gmail.com>
2024-05-11 02:12:18 -0700

    Actualizacion viernes
    notas completadas y clase busqueda creada para agregar una barra de busqueda en el boton de lupa

---
+++ lib/paginas/Busqueda.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BuscarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buscar'),
+      ),
+      body: Center(
+        child: Text('Página de búsqueda'),
+      ),
+    );
+  }
+}
